51e2a1ebc916fc0890c0d4873fb0b112b4b7f690
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-02-25 09:13:15 +0530

    Basic Draft

---
+++ Basic.js
@@ -0,0 +1,23 @@
+var num = 5 // in-line comment
+
+/*
+This is a multi line comment
+This is a multi line comment
+This is a multi line comment
+This is a multi line comment
+*/
+
+//---------------------------------------------------
+
+// Declaring Variables
+
+var myName = "Sree"
+
+myname = 80
+
+let myName = "Sreerag"
+
+const pi = 3.14
+
+//---------------------------------------------------
+
